7eb2bb0bfa37a7143af17f444f5ccc1938982d57
Adithya <adithyapantangi123@gmail.com>
2022-09-09 23:00:46 +0530

    Updated layout and added checkout button

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -8,29 +8,66 @@ const checkout = () => {
     <div
       style={{
         display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
+        justifyContent: "center",
+        // backgroundColor: "gray",
       }}
     >
-      Checking out...
-      {/* {cartItems.map((item: any) => (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          // backgroundColor: "whitesmoke",
+          height: "60vh",
+          width: "60%",
+        }}
+      >
+        {/* {cartItems.map((item: any) => (
         <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            backgroundColor: "black",
-            width: "300px",
-            borderRadius: "10px",
-          }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: "black",
+          width: "300px",
+          borderRadius: "10px",
+        }}
         >
-          <span style={{ color: "#f97316" }}>{item.name}</span>
-          <span style={{ color: "#f97316" }}>{item.quantity}</span>
+        <span style={{ color: "#f97316" }}>{item.name}</span>
+        <span style={{ color: "#f97316" }}>{item.quantity}</span>
         </div>
       ))} */}
-      {cartItems.map((item: any) => (
-        <ProjectCard id={item.id} name={item.name} quantity={item.quantity} />
-      ))}
+        {cartItems.length === 0 ? (
+          <span
+            style={{
+              fontSize: "1.25rem",
+              fontWeight: "bold",
+            }}
+          >
+            Cart is empty
+          </span>
+        ) : null}
+        {cartItems.map((item: any) => (
+          <ProjectCard id={item.id} name={item.name} quantity={item.quantity} />
+        ))}
+        {cartItems.length > 0 ? (
+          <button
+            style={{
+              alignSelf: "end",
+              height: "30px",
+              width: "90px",
+              borderRadius: "5px",
+              margin: "5px",
+              backgroundColor: "black",
+
+              color: "white",
+            }}
+          >
+            Checkout
+          </button>
+        ) : null}
+      </div>
     </div>
   );
 };
